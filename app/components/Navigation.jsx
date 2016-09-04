bf7a5a7eb60145bc50f90856397a3ff3d3f4fb7e
Kate Wood <katewood611@gmail.com>
2016-09-04 00:05:27 -0700

    adds navigation component to boilerplate app

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,35 @@
+var React = require('react')
+var {Link, IndexLink} = require('react-router')
+
+var Nav = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">
+              Timer
+            </IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">
+              Countdown
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            <a href="https://github.com/KateWood" target="_blank"><span style={{color: 'white'}}>Created by</span> Kate Wood</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+module.exports = Nav
